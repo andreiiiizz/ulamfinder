83d66ed83f39438fb89acc6e3533388dbd534e37
andreiiiizz <kurtandreiiii@gmail.com>
2026-08-11 22:19:01 +0800

    Remove google_search tool from API request
    Removed google_search tool from the request body.

--- api/recipe.js
+++ api/recipe.js
@@ -29,7 +29,6 @@ export default async function handler(req, res) {
       },
       body: JSON.stringify({
         contents: [{ parts: [{ text: prompt }] }],
-        tools: [{ google_search: {} }]
       })
     });
 
