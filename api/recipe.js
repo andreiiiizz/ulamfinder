abc52f089e60db6996633f2cf4c3f5cb24cf6ed6
andreiiiizz <kurtandreiiii@gmail.com>
2026-08-11 17:19:14 +0800

    Update recipe.js

--- api/recipe.js
+++ api/recipe.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   console.log('API called with method:', req.method);
   
   if (req.method !== 'POST') {
